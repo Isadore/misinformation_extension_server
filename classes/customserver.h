f8e3bc6d3893e255cc4deb9ca9d43e4981fa5d9c
remyrascal <37592530+remyrascal@users.noreply.github.com>
2022-05-22 03:44:14 -0400

    added default constructor

--- classes/customserver.h
+++ classes/customserver.h
@@ -1,8 +1,17 @@
+#ifndef customserver_h
+#define customserver_h
+
 #include "webserver.h"
+#include <iostream>
 
 class CustomServer : public WebServer {
 
     protected:
         virtual void listeners();
+    
+    public:
+        CustomServer(std::string ip, int port) : WebServer(ip, port) {}
 
 };
+
+#endif
