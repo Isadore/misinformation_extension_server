59d7ff6e56c9512f4ceb7b163f01b6a3863249e1
remyrascal <37592530+remyrascal@users.noreply.github.com>
2022-05-22 03:43:41 -0400

    changed to customserver

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include "./classes/webserver.h"
+#include "./classes/customserver.h"
 
 using namespace std;
 
 int main() {
 
-    WebServer server("127.0.0.1", 3000);
+    CustomServer server("127.0.0.1", 3000);
     server.start();
 
     return 0;
